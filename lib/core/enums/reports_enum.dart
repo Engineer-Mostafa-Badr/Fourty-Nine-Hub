9cebfcef034434d2d1afec1f428eb8e5070df9a7
Salama <01550526487m@gmail.com>
2024-07-31 17:01:45 +0300

    facebook work for display and add (posts,comments,replies)
    and finish suggested friends

--- lib/core/enums/reports_enum.dart
+++ lib/core/enums/reports_enum.dart
@@ -25,22 +25,43 @@ extension ReportsEnumExtention on ReportsEnum {
     }
   }
 
-  String get displayTitle {
+  String get displayTitleEn {
     switch (this) {
       case ReportsEnum.nudity:
-        return 'nudity';
+        return 'This content contains explicit nudity and is inappropriate for general audiences.';
       case ReportsEnum.frequent:
-        return 'frequent';
+        return 'Please investigate the frequency of this user\'s posts.';
       case ReportsEnum.fake:
-        return 'fake';
+        return 'The content in question is deceptive and should be verified for authenticity.';
       case ReportsEnum.abuse:
-        return 'abuse';
+        return 'The content reported involves abusive language or behavior.';
       case ReportsEnum.hated:
-        return 'hated';
+        return 'This report concerns content that promotes hate speech or discrimination.';
       case ReportsEnum.illegal:
-        return 'illegal';
+        return 'The content reported is suspected of promoting illegal activities.';
       case ReportsEnum.politics:
-        return 'politics';    }
+        return 'Illegal activities are strictly prohibited on our platform.';
+    }
+  }
+
+
+  String get displayTitleAr {
+    switch (this) {
+      case ReportsEnum.nudity:
+        return 'يحتوي هذا المحتوى على عري صريح وهو غير مناسب للجماهير العامة.';
+      case ReportsEnum.frequent:
+        return 'النشر المفرط لنفس الرسالة يعطل تجربة المستخدم.';
+      case ReportsEnum.fake:
+        return 'يبرز هذا التقرير انتشار الأخبار الزائفة أو المعلومات المضللة.';
+      case ReportsEnum.abuse:
+        return 'المحتوى المبلغ عنه يتضمن لغة أو سلوكاً مسيئاً.';
+      case ReportsEnum.hated:
+        return 'يتعلق هذا التقرير بمحتوى يروج لخطاب الكراهية أو التمييز.';
+      case ReportsEnum.illegal:
+        return 'المحتوى المبلغ عنه مشتبه في ترويجه لأنشطة غير قانونية.';
+      case ReportsEnum.politics:
+        return 'يتضمن المحتوى المعني دعاية سياسية أو معلومات متحيزة.';
+    }
   }
 
 }
