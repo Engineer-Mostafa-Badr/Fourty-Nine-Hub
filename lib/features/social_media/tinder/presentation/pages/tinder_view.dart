8e54fc3a3b6b94d0387515d44ae56c7d4a7143a3
tefaana616 <tefaana616@gmail.com>
2024-08-03 17:50:07 +0000

    Your commit message

--- lib/features/social_media/tinder/presentation/pages/tinder_view.dart
+++ lib/features/social_media/tinder/presentation/pages/tinder_view.dart
@@ -320,7 +320,7 @@ class TinderView extends StatelessWidget {
               height: MediaQuery.of(context).size.height / 1.5,
               child: const Padding(
                 padding: EdgeInsets.all(8.0),
-                child: ReportView(id: '2'),
+                child: ReportView(id: '2', categoryId: '',),
               ),
             ),
           );
@@ -343,4 +343,4 @@ class TinderView extends StatelessWidget {
     );
   }
 }
-//rommana1
+//rommana3
--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -60,6 +60,7 @@ import 'package:fourtyninehub/features/social_media/instagram/presentation/pages
 import 'package:fourtyninehub/features/social_media/live_streaming/presentation/pages/live_stream_view.dart';
 import 'package:fourtyninehub/features/social_media/reels/presentation/controllers/explore_reels_cubit/explore_reels_cubit.dart';
 import 'package:fourtyninehub/features/social_media/reels/presentation/pages/music_reels.dart';
+import 'package:fourtyninehub/features/social_media/reels/presentation/pages/reel_view.dart';
 import 'package:fourtyninehub/features/social_media/social_posts/presentation/cubit/social_posts_cubit.dart';
 import 'package:fourtyninehub/features/social_media/tinder/presentation/pages/tinder_view.dart';
 import 'package:fourtyninehub/features/social_media/twitter/presentation/bloc/twitter_bloc.dart';
@@ -137,7 +138,6 @@ import '../features/social_media/club_house/presentation/pages/club_house_home.d
 import '../features/social_media/club_house/presentation/widgets/clubHouseChat.dart';
 import '../features/social_media/club_house/presentation/widgets/clubHouseRoom.dart';
 import '../features/social_media/create_post/presentation/pages/create_post_view.dart';
-import '../features/social_media/reels/presentation/pages/Reel_view.dart';
 import '../features/social_media/social_posts/presentation/pages/Social_home.dart';
 import '../features/social_media/social_posts/presentation/pages/other_account_view.dart';
 import '../features/subcategories/presentation/cubit/subcategories_cubit.dart';
