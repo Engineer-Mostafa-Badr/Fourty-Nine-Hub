291ccf0c331aad4004406f3b7442eec05aeb2dc3
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-08-19 10:27:59 +0300

    Applying localization

--- lib/common/widgets/dynamic/bottom_navigator.dart
+++ lib/common/widgets/dynamic/bottom_navigator.dart
@@ -1,9 +1,11 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:fourtyninehub/core/extensions/context_extension.dart';
 import 'package:fourtyninehub/core/extensions/string_extension.dart';
+import 'package:fourtyninehub/core/localization/locale_keys.g.dart';
 import '../../../res/assets/assets.dart';
 import '../../../routes/routes.dart';
 import 'package:go_router/go_router.dart';
