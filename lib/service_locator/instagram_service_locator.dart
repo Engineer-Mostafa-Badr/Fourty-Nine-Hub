23af8feec32fc6b7afed804e1fd6cbed8b837c8e
Alimazen-2001 <alimazen2001ali@gmail.com>
2024-08-29 20:09:24 +0300

    solving instagram issues

--- lib/service_locator/instagram_service_locator.dart
+++ lib/service_locator/instagram_service_locator.dart
@@ -1,6 +1,7 @@
 import 'package:fourtyninehub/features/social_media/instagram/data/datasources/instagram_remote_datasource.dart';
 import 'package:fourtyninehub/features/social_media/instagram/data/repositories/instagram_repo_impl.dart';
 import 'package:fourtyninehub/features/social_media/instagram/domain/repositories/social_posts_repo.dart';
+import 'package:fourtyninehub/features/social_media/instagram/domain/usecases/get_instagram_feed_usecase.dart';
 import 'package:fourtyninehub/features/social_media/instagram/domain/usecases/get_instagram_reels_usecase.dart';
 import 'package:fourtyninehub/features/social_media/instagram/domain/usecases/get_user_reels_usecase.dart';
 import 'package:fourtyninehub/features/social_media/instagram/presentation/cubit/instagram_cubit.dart';
@@ -20,6 +21,9 @@ class InstagramServiceLocator {
 
 
     serviceLocator.registerLazySingleton<GetInstagramUserReelsUseCase>(() => GetInstagramUserReelsUseCase(
+      serviceLocator(),
+    ));
+    serviceLocator.registerLazySingleton<GetInstagramFeedUseCase>(() => GetInstagramFeedUseCase(
       serviceLocator(),
     ));
 
