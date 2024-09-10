a24ec9001fb6822ed18d76bffdf1555ce148fe61
Allam <abdelrhmnallam@gmail.com>
2024-09-10 22:27:37 +0300

    merge with origin feature/chat

--- lib/features/social_media/chat/chat_view/presentation/chat_cubit/chats_cubit.dart
+++ lib/features/social_media/chat/chat_view/presentation/chat_cubit/chats_cubit.dart
@@ -4,12 +4,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fourtyninehub/core/abstract/use_case.dart';
 import 'package:fourtyninehub/core/enums/chat_categories.dart';
 import 'package:fourtyninehub/core/error/failure.dart';
-import 'package:fourtyninehub/core/data/datasources/remote/socket/socket_data_source.dart';
 import 'package:fourtyninehub/features/social_media/chat/chat_room/data/models/seen_history_model.dart';
 import 'package:fourtyninehub/features/social_media/chat/chat_room/domain/entities/message_entity.dart';
 import 'package:fourtyninehub/features/social_media/chat/chat_room/domain/usecases/listen_to_new_message_usecase.dart';
 import 'package:fourtyninehub/features/social_media/chat/chat_room/domain/usecases/stop_listen_to_messages.dart';
-import 'package:fourtyninehub/features/social_media/chat/chat_view/data/models/chat_model.dart';
 import 'package:fourtyninehub/features/social_media/chat/chat_view/domain/entities/chat_entity.dart';
 import 'package:fourtyninehub/features/social_media/chat/chat_view/domain/usecases/get_chats_usecase.dart';
 
@@ -35,72 +33,18 @@ class ChatsCubit extends Cubit<ChatsState> {
     getSocialChats();
   }
 
-  initChat() async {
-    // await getChats(category: ChatCategories.social);
-  }
-
   Future<void> getSocialChats() async {
     final result = await _getChatsUseCase(
         GetChatsParams(categoryId: ChatCategoriesIds.social));
     result.fold(
         (l) => emit(state.copyWith(status: ChatsStates.error, failure: l)),
         (chats) {
-          for(final chat in chats){
-            _chats[chat.id] = chat;
-          }
-          emit(state.copyWith(chats: chats, status: ChatsStates.success));
-        });
+      for (final chat in chats) {
+        _chats[chat.id] = chat;
+      }
+      emit(state.copyWith(chats: chats, status: ChatsStates.success));
+    });
   }
-
-  // getChats({required ChatCategories category, String? password}) async {
-  //   selectedChatCategory = category;
-  //
-  //   // if this locked chat tab & password null return empty list
-  //   if (category == ChatCategories.locked && password == null) {
-  //     return emit.call(state.copyWith(chats: [], status: ChatsStates.success));
-  //   }
-  //   GetChatsParams chatsRequestParams = GetChatsParams(
-  //       categoryId: ChatCategoriesIds.social, privacy: ChatPrivacy.normal);
-  //   // getTabParams(category: category, password: password);
-  //
-  //   if (chatsRequestParams.categoryId == null &&
-  //       category != ChatCategories.groups) {
-  //     return emit.call(state.copyWith(chats: [], status: ChatsStates.success));
-  //   } else {
-  //     _chats.clear();
-  //     var response;
-  //     // if (category == ChatCategories.groups) {
-  //     //   response = await _groupsChatsUseCase.call(const NoParams());
-  //     // } else {
-  //     response = await _getChatsUseCase.call(
-  //       chatsRequestParams,
-  //     );
-  //     // }
-  //
-  //     response.fold(
-  //         (failure) => emit.call(
-  //             state.copyWith(failure: failure, status: ChatsStates.error)),
-  //         (data) async {
-  //       data.chats!
-  //           .map((e) => _chats.update(e.id!, (value) => e, ifAbsent: () => e))
-  //           .toList();
-  //
-  //       // to listen typing and online emit event status
-  //       List<UserStatusParams> userStatusParams = [];
-  //       for (var item in _chats.values) {
-  //         userStatusParams
-  //             .add(UserStatusParams(chatId: item.id, userId: item.userId));
-  //       }
-  //
-  //       await Future.delayed(const Duration(seconds: 1));
-  //       sendUserStatus(userStatusParams);
-  //       unReadMessage = 0;
-  //
-  //       return emit.call(
-  //           state.copyWith(chats: data.chats, status: ChatsStates.success));
-  //     });
-  //   }
-  // }
 
   _listenToNewMessages() {
     _listenToNewMessageUseCase((message) {
