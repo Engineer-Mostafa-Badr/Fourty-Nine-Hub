d72ddd0f079bddd330e912038170a8fa82ec82a8
Alimazen-2001 <alimazen2001ali@gmail.com>
2024-09-19 16:39:59 +0300

    adding base arch for live integ

---
+++ lib/features/social_media/live_streaming/domain/usecases/create_live_use_case.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:fourtyninehub/core/error/failure.dart';
+
+import '../../../../../core/abstract/use_case.dart';
+import '../entity/live_create_response.dart';
+import '../repository/live_repository.dart';
+
+class CreateLiveUseCase extends UseCase<LiveCreateResponse, NoParams> {
+  final LiveRepository _liveRepository;
+
+  CreateLiveUseCase({required LiveRepository liveRepository})
+      : _liveRepository = liveRepository;
+  @override
+  Future<Either<Failure, LiveCreateResponse>> call(NoParams params) {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+}
