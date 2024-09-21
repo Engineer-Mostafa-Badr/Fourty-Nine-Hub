a671d8da8cfbb50c40565fda0f40a3dc167dfbf1
Allam <abdelrhmnallam@gmail.com>
2024-09-21 14:26:05 +0300

    last commit from Allam

--- lib/features/authentication/presentation/pages/login_view.dart
+++ lib/features/authentication/presentation/pages/login_view.dart
@@ -11,6 +11,7 @@ import 'package:fourtyninehub/common/widgets/stateless/labels/badged_label.dart'
 import 'package:fourtyninehub/core/extensions/string_extension.dart';
 import 'package:fourtyninehub/core/localization/locale_keys.g.dart';
 import 'package:fourtyninehub/core/messages/messages.dart';
+import 'package:fourtyninehub/core/service/background_service.dart';
 import 'package:fourtyninehub/core/utils/shared_pref.dart';
 import 'package:fourtyninehub/features/account_taps/wallet/presentation/cubit/wallet_cubit.dart';
 import 'package:fourtyninehub/features/authentication/presentation/controllers/register_cubit/register_cubit.dart';
@@ -98,7 +99,7 @@ class _LoginViewState extends State<LoginView> {
           } else if (state is LoginSuccess) {
             await TokenManager.saveAccessToken(state.userTokensEntity.accessToken);
             await TokenManager.saveRefreshToken(state.userTokensEntity.refreshToken);
-
+            await BackgroundService.reStartWebSocketService(state.userTokensEntity.accessToken);
             serviceLocator<UserCubit>()
               ..setLogin(true)
               ..attachToken()
