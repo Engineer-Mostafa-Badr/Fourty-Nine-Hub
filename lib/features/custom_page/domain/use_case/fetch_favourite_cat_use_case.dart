af533c7f55976b837a1fcd5770f1293f46bf6606
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-09-25 13:28:19 +0300

    Create favourite category

--- lib/features/custom_page/domain/use_case/fetch_favourite_cat_use_case.dart
+++ lib/features/custom_page/domain/use_case/fetch_favourite_cat_use_case.dart
@@ -4,7 +4,6 @@ import 'package:fourtyninehub/core/error/failure.dart';
 import 'package:fourtyninehub/features/custom_page/domain/reposiory/custom_page_repository.dart';
 
 import '../entity/favourite_categ_entity.dart';
-import '../entity/navigate_bar_entity.dart';
 
 class FetchFavouriteCatUseCase extends UseCase<FavouriteCatEntity,NoParams>{
   final CustomPageRepository _customPageRepository;
