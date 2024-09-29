8c30833f865c39bf542a8adc2642660cb6cee278
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-09-30 01:23:05 +0300

    Applying delete and subscription on trip join

--- lib/features/account_taps/my_adds/domain/usecases/delete_my_trip_join_usecase.dart
+++ lib/features/account_taps/my_adds/domain/usecases/delete_my_trip_join_usecase.dart
@@ -1,9 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:fourtyninehub/core/error/failure.dart';
-import 'package:fourtyninehub/features/account_taps/my_adds/domain/entity/my_ads_trip_join_entity.dart';
 import 'package:fourtyninehub/features/account_taps/my_adds/domain/repositories/my_ads_repo.dart';
 import '../../../../../../core/abstract/use_case.dart';
-import '../entity/my_ads_auction.dart';
 
 class DeleteMyTripJoinUseCase extends UseCase<bool, String> {
   final MyAdsRepo _repo;
--- lib/features/account_taps/my_adds/presentation/pages/my_adds.dart
+++ lib/features/account_taps/my_adds/presentation/pages/my_adds.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fourtyninehub/common/widgets/stateful/banners/back_appbar.dart';
-import 'package:fourtyninehub/common/widgets/stateless/dynamic/are_you_sure.dart';
 import 'package:fourtyninehub/common/widgets/stateless/pages/empty.dart';
 import 'package:fourtyninehub/core/extensions/string_extension.dart';
 import 'package:fourtyninehub/core/localization/locale_keys.g.dart';
