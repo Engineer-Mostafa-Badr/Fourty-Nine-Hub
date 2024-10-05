66fbfd59713ca85b973236e368fe8e5550631ef5
AtefMMO <mohamedatefimb@gmail.com>
2024-10-05 17:06:22 +0300

    start

---
+++ lib/features/ride/rider_shipping/presentation/pages/create_trip_rider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateTripRider extends StatelessWidget {
+  const CreateTripRider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
