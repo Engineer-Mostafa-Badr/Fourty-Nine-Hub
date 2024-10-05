d501c98ffbadbd9e9120c7d33a85404365a9a929
Mohamed Taha Hassen <mt337848@gmail.com>
2024-10-05 19:50:43 +0300

    subscribe view 60& done

--- lib/features/chance_feature/presentation/widgets/chance_card_widget.dart
+++ lib/features/chance_feature/presentation/widgets/chance_card_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fourtyninehub/features/chance_feature/presentation/pages/chance_details_view.dart';
 import 'package:fourtyninehub/features/chance_feature/presentation/widgets/image_card_widget.dart';
-import 'package:fourtyninehub/features/chance_feature/presentation/widgets/not_subscribed_widget.dart';
+import 'package:fourtyninehub/features/chance_feature/presentation/widgets/subscribe_widget_in_card.dart';
 import 'package:fourtyninehub/features/chance_feature/presentation/widgets/slider_card_widget.dart';
 
 import '../../../../res/style/app_colors.dart';
