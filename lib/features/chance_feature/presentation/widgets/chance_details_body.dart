c6b9f420dfeeb7e3373048ab6e1211a5c5b02bc0
Mohamed Taha Hassen <mt337848@gmail.com>
2024-10-06 17:27:26 +0300

    refactor chance details view

---
+++ lib/features/chance_feature/presentation/widgets/chance_details_body.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../res/style/styles.dart';
+import '../widgets/card_details_widget_of_details_view.dart';
+import '../widgets/counter_money_widget.dart';
+import '../widgets/subscribe_button_widget.dart';
+
+class ChanceDetailsBody extends StatelessWidget {
+  const ChanceDetailsBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const CardDetails(),
+        SizedBox(height: 20.h),
+        Text(
+            'Subscribe',
+            style: Styles.mediumText(
+                color: Theme.of(context).primaryColor,
+                fontSize: 70.sp,
+                fontWeight: FontWeight.bold
+            )),
+        SizedBox(height: 10.h),
+        Text(
+            'Type the value you want to participation',
+            style: Styles.mediumText(
+              color: Theme.of(context).primaryColor,
+              fontSize: 50.sp,
+
+            )),
+        SizedBox(height: 15.h),
+        const CounterMoneyWidget(),
+        SizedBox(height: 80.h),
+        const SubscribeButtonWidget(),
+      ],
+    );
+  }
+}
---
+++ lib/features/chance_feature/presentation/widgets/counter_money_widget.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:fourtyninehub/core/extensions/context_extension.dart';
+
+import '../../../../res/style/app_colors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../res/style/styles.dart';
+class CounterMoneyWidget extends StatelessWidget {
+  const CounterMoneyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8)),
+              padding:  EdgeInsets.symmetric(
+                horizontal: 15.w, vertical: 20.h,
+              ),
+              backgroundColor: AppColors.PRIMARY_COLOR,
+            ),
+            child: Icon(
+              Icons.remove,
+              color: Theme.of(context).scaffoldBackgroundColor,
+            )),
+        const Spacer(),
+        Container(
+          width: context.screenWidth / 1.7,
+          height: 70.h,
+          padding:
+          EdgeInsets.only(top: 3.h, bottom: 3.h, left: 2.w, right: 25.w),
+          decoration: BoxDecoration(
+            color: AppColors.PRIMARY_COLOR,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.close, color: Colors.white),
+                onPressed: () {},
+              ),
+              const Spacer(),
+              Text('0',
+                  style: Styles.mediumText(
+                    color: Theme.of(context).scaffoldBackgroundColor,
+                    fontSize: 80.sp,
+                  )),
+            ],
+          ),
+        ),
+        const Spacer(),
+        ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8)),
+              padding:  EdgeInsets.symmetric(
+                horizontal: 15.w, vertical: 20.h,
+              ),
+              backgroundColor: AppColors.PRIMARY_COLOR,
+            ),
+            child: Icon(
+              Icons.add,
+              color: Theme.of(context).scaffoldBackgroundColor,
+            ))
+      ],
+    );
+  }
+}
