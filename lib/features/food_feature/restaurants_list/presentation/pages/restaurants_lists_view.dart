577820af71872b8d9c7aaf1849d591d09b4c3e4c
Salama <mebrahim15111@gmail.com>
2024-10-30 12:51:41 +0200

    fix food feature(Home)

--- lib/features/food_feature/restaurants_list/presentation/pages/restaurants_lists_view.dart
+++ lib/features/food_feature/restaurants_list/presentation/pages/restaurants_lists_view.dart
@@ -1,6 +1,5 @@
 // For JSON decoding
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
