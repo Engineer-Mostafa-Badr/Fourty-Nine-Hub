3ffb5f9f111c4063118680aa1c9ed31115ddd988
Salama <mebrahim15111@gmail.com>
2024-12-31 20:49:25 +0200

    car pool

--- lib/features/star_feature/domain/entity/banner_talent_entity.dart
+++ lib/features/star_feature/domain/entity/banner_talent_entity.dart
@@ -1,5 +1,7 @@
 class BannerTalentEntity{
   final String banner;
+  final String title;
+  final String subTitle;
 
-  BannerTalentEntity({required this.banner});
+  BannerTalentEntity({required this.banner,required this.title,required this.subTitle});
 }
