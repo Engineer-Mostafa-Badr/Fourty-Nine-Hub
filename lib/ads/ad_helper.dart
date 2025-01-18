8e094a03d9f4e25180412e0d1d1626b54ed54400
ahmedamer210 <eng.aboali712@gmail.com>
2025-01-18 18:09:41 +0200

    Merge remote-tracking branch 'origin/fix_bugs' into fix_bugs

--- lib/ads/ad_helper.dart
+++ lib/ads/ad_helper.dart
@@ -1,17 +1,17 @@
 class AdHelper {
   static String get bannerAdUnitId {
-    return 'ca-app-pub-3940256099942544/63009781113';
+    return 'ca-app-pub-3940256099942544/6300978111';
   }
 
   static String get InterstitialAd {
-    return "ca-app-pub-3940256099942544/10331737123";
+    return "ca-app-pub-3940256099942544/1033173712";
   }
 
   static String get OpenAppAd {
-    return "ca-app-pub-3940256099942544/92573959213";
+    return "ca-app-pub-3940256099942544/9257395921";
   }
 
   static String get nativeAdUnitId {
-    return "ca-app-pub-3940256099942544/22476961103"; // Test Native Ad ID
+    return "ca-app-pub-3940256099942544/2247696110"; // Test Native Ad ID
   }
 }
