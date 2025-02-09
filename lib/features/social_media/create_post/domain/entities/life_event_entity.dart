8cc075574fa66f088dac69706caed1293b2b68ba
Salama <mebrahim15111@gmail.com>
2025-02-09 17:20:08 +0200

    question feature

---
+++ lib/features/social_media/create_post/domain/entities/life_event_entity.dart
@@ -0,0 +1,8 @@
+class LifeEventEntity{
+  final String id;
+  final String titleAr;
+  final String titleEn;
+  final String image;
+
+  LifeEventEntity({required this.id,required this.titleAr,required this.titleEn,required this.image});
+}
