2b19df127e6e42433d7c439a00504cd6cbbddacc
montasermohamed101 <montasermohamed101@gmail.com>
2025-04-02 21:19:48 +0200

    after develompent

--- lib/features/food_feature/restaurants_list/presentation/pages/widgets/restaurant_list/meal_category_card.dart
+++ lib/features/food_feature/restaurants_list/presentation/pages/widgets/restaurant_list/meal_category_card.dart
@@ -39,61 +39,112 @@ class _MealCategoryCardState extends State<MealCategoryCard> {
           child: InkWell(
             onTap: () => widget.onTap(widget.subCategory?.id ?? ""),
             child: Column(
-              mainAxisSize: MainAxisSize.min,
+              // mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Stack(
-                  children: [
-                    // Heart image
-                    widget.subCategory?.fromAsset == true
-                        ? Image.asset(
-                            widget.subCategory?.image ?? '',
-                            fit: BoxFit.fill,
-                            height: 300.h,
-                            width: 0.55.sw,
-                          )
-                        : ImageFromInternet(
-                            image: widget.subCategory?.picture ?? '',
-                            defaultLogo: true,
-                            height: 300.h,
-                            width: 0.55.sw,
-                          ),
-                    if (context.read<UserCubit>().isLoggedIn &&
-                        widget.subCategory?.id != '')
-                      Positioned(
-                        top: 5,
-                        right: 5,
-                        child: IconAppButton(
-                            size: 25,
-                            icon: widget.subCategory?.isFavorite ?? false
-                                ? Icons.favorite
-                                : Icons.favorite_border,
-                            color: AppColors.PRIMARY_COLOR_DARK,
-                            onPressed: () async {
-                              await widget.favouriteSubCategory();
-                            }),
-                      ),
-                  ],
-                ),
                 Container(
-                  width: double.infinity,
-                  color: widget.subCategory?.isSelected == true
-                      ? AppColors.SECONDARY_COLOR
-                      : cardDarkColor(context),
-                  padding: EdgeInsetsDirectional.only(
-                      top: 8.h, end: 10.w, start: 16.w, bottom: 16.h),
-                  child: Label(
-                    text: (getLang() == "ar"
-                            ? widget.subCategory?.nameAr
-                            : widget.subCategory?.nameEn) ??
-                        "",
-                    style: Styles.headerText(
-                      color: widget.subCategory?.isSelected == true
-                          ? Colors.white
-                          : null,
-                    ),
+                  decoration: BoxDecoration(
+                    color:widget.subCategory?.isSelected == true
+                        ? AppColors.SECONDARY_COLOR:
+                    Colors.grey,
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(width: 0.5, color: AppColors.black),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    // This makes children fill the width
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(
+                          15,
+                        ),
+                        // Slightly less than container radius to account for border
+                        child: widget.subCategory?.fromAsset == true
+                            ? Image.asset(
+                                widget.subCategory?.image ?? '',
+                                fit: BoxFit.cover,
+                                // This makes the image fill the space
+                                // height: 300.h,
+                                width: double.infinity, // Takes full width
+                              )
+                            : ImageFromInternet(
+                                image: widget.subCategory?.picture ?? '',
+                                defaultLogo: true,
+                                fit: BoxFit.cover,
+                                height: 300.h,
+                                width: double.infinity,
+                              ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        // Add some padding around the title
+                        child:  Label(
+                          text: (getLang() == "ar"
+                              ? widget.subCategory?.nameAr
+                              : widget.subCategory?.nameEn) ??
+                              "",
+                          style: Styles.headerText(
+                            color: widget.subCategory?.isSelected == true
+                                ? Colors.white
+                                : null,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
+                // Stack(
+                //   children: [
+                //     // Heart image
+                //     widget.subCategory?.fromAsset == true
+                //         ? Image.asset(
+                //             widget.subCategory?.image ?? '',
+                //             fit: BoxFit.fill,
+                //             height: 300.h,
+                //             width: 0.55.sw,
+                //           )
+                //         : ImageFromInternet(
+                //             image: widget.subCategory?.picture ?? '',
+                //             defaultLogo: true,
+                //             height: 300.h,
+                //             width: 0.55.sw,
+                //           ),
+                //     if (context.read<UserCubit>().isLoggedIn &&
+                //         widget.subCategory?.id != '')
+                //       Positioned(
+                //         top: 5,
+                //         right: 5,
+                //         child: IconAppButton(
+                //             size: 25,
+                //             icon: widget.subCategory?.isFavorite ?? false
+                //                 ? Icons.favorite
+                //                 : Icons.favorite_border,
+                //             color: AppColors.PRIMARY_COLOR_DARK,
+                //             onPressed: () async {
+                //               await widget.favouriteSubCategory();
+                //             }),
+                //       ),
+                //   ],
+                // ),
+                // Container(
+                //   width: double.infinity,
+                //   color: widget.subCategory?.isSelected == true
+                //       ? AppColors.SECONDARY_COLOR
+                //       : cardDarkColor(context),
+                //   padding: EdgeInsetsDirectional.only(
+                //       top: 8.h, end: 10.w, start: 16.w, bottom: 16.h),
+                //   child: Label(
+                //     text: (getLang() == "ar"
+                //             ? widget.subCategory?.nameAr
+                //             : widget.subCategory?.nameEn) ??
+                //         "",
+                //     style: Styles.headerText(
+                //       color: widget.subCategory?.isSelected == true
+                //           ? Colors.white
+                //           : null,
+                //     ),
+                //   ),
+                // ),
               ],
             ),
           ),
