26307983a17888385273ede9eb0737f0c9929353
Ahmed Nasr <ahmed.nasr.fahmey@gmail.com>
2025-04-24 00:56:16 +0200

    Finish maps on trip join, captain share, pick meee

--- lib/features/trip_join/add_new_pick_me/presentation/views/add_new_pick_me_view.dart
+++ lib/features/trip_join/add_new_pick_me/presentation/views/add_new_pick_me_view.dart
@@ -160,6 +160,7 @@ class _AddNewPickMeViewState extends State<AddNewPickMeView> {
                       padding: EdgeInsets.only(top: 8.0.h, bottom: 20.h),
                       child: const PremiumAndRequestWidget(),
                     ),
+
                   ],
                 ),
               ),
