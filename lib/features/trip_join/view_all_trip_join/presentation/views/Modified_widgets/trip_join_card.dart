df732d94a6fdfc85bc4b9c1ab5fe1cc4f13d3a9a
moelhlaby <moel7laby@gmail.com>
2025-05-02 00:37:20 +0300

    fixing bugs in captain share & trip join ui

--- lib/features/trip_join/view_all_trip_join/presentation/views/Modified_widgets/trip_join_card.dart
+++ lib/features/trip_join/view_all_trip_join/presentation/views/Modified_widgets/trip_join_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fourtyninehub/core/extensions/context_extension.dart';
 import 'package:fourtyninehub/res/style/app_colors.dart';
 
