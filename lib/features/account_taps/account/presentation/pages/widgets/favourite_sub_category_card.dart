d3f260ba171e1c48a475a38428ccf1d065a7ba57
muhammadessam <muhammad159e@gmail.com>
2025-05-01 06:00:23 +0300

    edit marriage

--- lib/features/account_taps/account/presentation/pages/widgets/favourite_sub_category_card.dart
+++ lib/features/account_taps/account/presentation/pages/widgets/favourite_sub_category_card.dart
@@ -5,6 +5,7 @@ import 'package:fourtyninehub/common/widgets/dynamic/sizer.dart';
 import 'package:fourtyninehub/common/widgets/stateless/buttons/iconAppButton.dart';
 import 'package:fourtyninehub/common/widgets/stateless/images/square_image.dart';
 import 'package:fourtyninehub/common/widgets/stateless/labels/label.dart';
+import 'package:fourtyninehub/core/extensions/context_extension.dart';
 import 'package:fourtyninehub/features/ads_feature/ads/presentation/pages/ads_view.dart';
 import 'package:fourtyninehub/features/ads_feature/create_ad/domain/entities/categorization_entity.dart';
 import 'package:fourtyninehub/features/fourty_nine/domain/entities/main_category_entity.dart';
@@ -47,7 +48,7 @@ class _FavouriteSubCategoryCardState extends State<FavouriteSubCategoryCard> {
       ),
       child: Container(
         decoration: BoxDecoration(
-          color: HexColor('E6E7EB'),
+          color: context.isDarkMode? AppColors.QUANTITY_COLOR : HexColor('E6E7EB'),
           borderRadius: BorderRadius.circular(30.r),
         ),
         clipBehavior: Clip.antiAliasWithSaveLayer,
@@ -90,7 +91,7 @@ class _FavouriteSubCategoryCardState extends State<FavouriteSubCategoryCard> {
                           text: context.locale == Locales.english
                               ? widget.item.nameEn
                               : widget.item.nameAr,
-                          style: Styles.mediumText(fontWeight: FontWeight.bold),
+                          style: Styles.mediumText(fontWeight: FontWeight.bold,color: context.isDarkMode? Colors.white : Colors.black),
                         ),
                         // Label(
                         //   text:
@@ -102,7 +103,7 @@ class _FavouriteSubCategoryCardState extends State<FavouriteSubCategoryCard> {
                   ),
                   IconAppButton(
                       icon: Icons.add_box_outlined,
-                      color: Theme.of(context).primaryColor,
+                      color: context.isDarkMode? Colors.white : AppColors.PRIMARY_COLOR,
                       size: 40.h,
                       onPressed: () {
                         if (AuthHelper().isLoggedIn()) {
