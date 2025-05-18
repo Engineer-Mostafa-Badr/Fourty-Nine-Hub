524daf0e871b9ec933f40e8ddabb50dac1bf509a
moelhlaby <moel7laby@gmail.com>
2025-05-18 19:43:09 +0300

    working on bugs

--- lib/features/notifications/presentation/widgets/response_status_builder.dart
+++ lib/features/notifications/presentation/widgets/response_status_builder.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fourtyninehub/common/widgets/dynamic/sizer.dart';
@@ -28,6 +29,7 @@ class _ResponseStatusBuilderState extends State<ResponseStatusBuilder> {
 
   @override
   Widget build(BuildContext context) {
+    final _ = context.locale;/// بيجبر الwidgets تعمل rebuild لما اللغة تتغير (setState لما اللغه تتغير)
     return BlocConsumer<GetStatusAllServicesNotificationsCubit,
         GetStatusAllServicesNotificationsState>(
       listener: (context, state) {
