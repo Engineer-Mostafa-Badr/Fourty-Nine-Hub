37fbe19df06ad2d33d0be2a0f5822d1c21202d72
Ahmed Nasr <ahmed.nasr.fahmey@gmail.com>
2025-07-15 22:35:26 +0300

    updates on map

--- lib/core/widget/counter_widget.dart
+++ lib/core/widget/counter_widget.dart
@@ -19,19 +19,19 @@ class CounterWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: width??38.w,
-      height: height??38.w,
+      width: width??42.w,
+      height: height??42.w,
       // padding: const EdgeInsets.all(2),
       decoration: BoxDecoration(
           border: Border.all(
-            color: borderColor??AppColors.getReversedTextColor(context),
-            width: borderWidth??1.w
+            color: AppColors.whiteColor,
+            width: borderWidth??3.w
           ),
           shape: BoxShape.circle, color:bgColor?? AppColors.getRedColor(context)),
       alignment: AlignmentDirectional.center,
       child: AutoSizeText(
-        formatNumber(1000,isArabic: context.isArabic),
-          style: TextStyle(fontSize: fontSize??4.sp,color:txtColor??AppColors.getReversedTextColor(context),),
+        formatNumber(unreadCount,isArabic: context.isArabic),
+          style: TextStyle(fontSize: 16.sp,color:txtColor??AppColors.getReversedTextColor(context),),
           maxLines: 1,
         minFontSize: 6,
         stepGranularity: 0.5,
