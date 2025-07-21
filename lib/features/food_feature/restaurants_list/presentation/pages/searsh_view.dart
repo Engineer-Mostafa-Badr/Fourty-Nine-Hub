ddc9d98fc8db4be073303f369a9d3853b0ace16b
muhammadessam <muhammad159e@gmail.com>
2025-07-22 00:00:44 +0300

    olx pagination v5

--- lib/features/food_feature/restaurants_list/presentation/pages/searsh_view.dart
+++ lib/features/food_feature/restaurants_list/presentation/pages/searsh_view.dart
@@ -25,10 +25,11 @@ import '../../../../../core/widget/olx_pagination/banner.dart';
 import '../../../../../core/widget/olx_pagination/olx_pagination_widget.dart';
 
 class SearchRestaurantView extends StatelessWidget {
-  const SearchRestaurantView({super.key, this.onClose});
+  const SearchRestaurantView({super.key, this.onClose, this.focusNode});
 
   final VoidCallback? onClose; // Callback to hide search UI
   final FocusNode? focusNode;
+
   @override
   Widget build(BuildContext context) {
     final searchCubit = context.read<SearchRestaurantsCubit>();
@@ -136,8 +137,8 @@ class SearchRestaurantView extends StatelessWidget {
                         banners: bannersList,
                         items: List.generate(
                           state.mealCategories?.length ?? 0,
-                              (index) {
-                                FoodCategoryEntity? category =
+                          (index) {
+                            FoodCategoryEntity? category =
                                 state.mealCategories?[index];
                             return GestureDetector(
                               onTap: () =>
@@ -148,26 +149,26 @@ class SearchRestaurantView extends StatelessWidget {
                                     imageUrl: category?.picture ?? "",
                                     imageBuilder: (context, imageProvider) =>
                                         Container(
-                                          width: 150.w,
-                                          height: 150.h,
-                                          alignment: Alignment.center,
-                                          decoration: BoxDecoration(
-                                              shape: BoxShape.circle,
-                                              color: Colors.grey.shade300,
-                                              boxShadow: [
-                                                BoxShadow(
-                                                  color: AppColors.PRIMARY_COLOR
-                                                      .withOpacity(0.5),
-                                                  spreadRadius: 2,
-                                                  blurRadius: 3,
-                                                  offset: const Offset(0, 3),
-                                                ),
-                                              ],
-                                              image: DecorationImage(
-                                                image: imageProvider,
-                                                fit: BoxFit.cover,
-                                              )),
-                                        ),
+                                      width: 150.w,
+                                      height: 150.h,
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color: Colors.grey.shade300,
+                                          boxShadow: [
+                                            BoxShadow(
+                                              color: AppColors.PRIMARY_COLOR
+                                                  .withOpacity(0.5),
+                                              spreadRadius: 2,
+                                              blurRadius: 3,
+                                              offset: const Offset(0, 3),
+                                            ),
+                                          ],
+                                          image: DecorationImage(
+                                            image: imageProvider,
+                                            fit: BoxFit.cover,
+                                          )),
+                                    ),
                                     width: 150.w,
                                   ),
                                   const Sizer(),
@@ -181,7 +182,7 @@ class SearchRestaurantView extends StatelessWidget {
                         ),
                       ),
                     );
-                   /* return ListView.separated(
+                    /* return ListView.separated(
                       itemCount: state.mealCategories?.length ?? 0,
                       padding: EdgeInsets.all(10.w),
                       separatorBuilder: (context, index) => Sizer(
@@ -240,7 +241,7 @@ class SearchRestaurantView extends StatelessWidget {
                   } else if (state.status ==
                       SearchRestaurantStates.loadingSearchSubCategory) {
                     return SizedBox(
-                      height: MediaQuery.of(context).size.height*.7,
+                      height: MediaQuery.of(context).size.height * .7,
                       child: RefreshIndicator(
                         onRefresh: () async => searchCubit.refreshState(),
                         child: OlxPaginationWidget(
