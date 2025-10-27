799c61cae5b1edd21ed107086cbf7821c14f36ee
zoomezia <zoomezia@gmail.com>
2025-10-27 16:34:11 +0400

    edit most booking model for display all data

--- lib/features/health_feature/health/data/models/most_booking_model.dart
+++ lib/features/health_feature/health/data/models/most_booking_model.dart
@@ -29,10 +29,10 @@ class MostBookingModel extends MostBookingEntity {
     if (json == null) return MostBookingModel();
 
     return MostBookingModel(
-      id: json['_id'],
-      firstName: json['firstName'],
-      lastName: json['lastName'],
-      userId: json['userId'],
+      id: json['_id'] ?? json['id'] ?? '',
+      firstName: json['firstName'] ?? '',
+      lastName: json['lastName'] ?? '',
+      userId: json['userId'] ?? json['id'] ?? '',
       address: json['address'] != null
           ? AddressModel.fromJson(json['address'])
           : null,
@@ -40,32 +40,49 @@ class MostBookingModel extends MostBookingEntity {
           ? MostSubCategoryModel.fromJson(json['subCategoryId'])
           : (json['medicalSpecialization'] != null
               ? MostSubCategoryModel.fromJson(json['medicalSpecialization'])
-              : null),
-      averageRating: json['averageRating']?.toDouble(),
-      totalRatings: json['totalRatings'],
-      ratingText: json['ratingText'],
-      bookingCount: json['bookingCount'],
-      viewCount: json['viewCount'],
+              : (json['speciality'] != null
+                  ? MostSubCategoryModel.fromJson(json['speciality'])
+                  : null)),
+      averageRating:
+          json['averageRating']?.toDouble() ?? json['rating']?.toDouble(),
+      totalRatings: json['totalRatings'] ?? 0,
+      ratingText: json['ratingText'] ?? '',
+      bookingCount: json['bookingCount'] ?? 0,
+      viewCount: json['viewCount'] ?? 0,
       profilePicture: json['mediaId'] != null && json['mediaId'] is Map
           ? json['mediaId']['mediaKey'] ?? json['mediaId']['_id']
-          : json['profilePicture'],
-      subscriptionType: json['subscriptionType'],
-      subscriptionRank: json['subscriptionRank'],
+          : json['profilePicture'] ?? json['image'] ?? '',
+      subscriptionType: json['subscriptionType'] ?? json['status'] ?? '',
+      subscriptionRank:
+          json['subscriptionRank'] ?? (json['status'] == 'pending' ? 0 : 1),
       waitingTimeAr: json['waitingTime'] != null
           ? (json['waitingTime'] is Map
               ? json['waitingTime']['ar']?.toString()
               : json['waitingTime'].toString())
-          : '',
+          : (json['detections'] != null &&
+                  json['detections'] is List &&
+                  (json['detections'] as List).isNotEmpty
+              ? '${(json['detections'] as List).first['detectionPeriod']} دقيقة'
+              : ''),
       waitingTimeEn: json['waitingTime'] != null
           ? (json['waitingTime'] is Map
               ? json['waitingTime']['en']?.toString()
               : json['waitingTime'].toString())
-          : '',
-      currencyAr: json['currencyAr']?.toString(),
-      currencyEn: json['currencyEn']?.toString(),
-      isPremium: json['isPremium'],
-      appointmentType: json['appointmentType'],
-      price: json['price'],
+          : (json['detections'] != null &&
+                  json['detections'] is List &&
+                  (json['detections'] as List).isNotEmpty
+              ? '${(json['detections'] as List).first['detectionPeriod']} minutes'
+              : ''),
+      currencyAr: json['currencyAr']?.toString() ?? 'جنيه',
+      currencyEn: json['currencyEn']?.toString() ?? 'EGP',
+      isPremium: json['isPremium'] ?? false,
+      appointmentType: json['appointmentType'] ?? 'clinic_visit',
+      price: json['price'] ??
+          (json['detections'] != null &&
+                  json['detections'] is List &&
+                  (json['detections'] as List).isNotEmpty
+              ? (json['detections'] as List).first['price']
+              : null),
     );
   }
 }
@@ -89,7 +106,7 @@ class AddressModel extends AddressEntity {
               ? MostCityModel.fromJson(json['city'])
               : MostCityModel(id: json['city'].toString()))
           : null,
-      address: json['address'],
+      address: json['address'] ?? '',
     );
   }
 }
@@ -104,10 +121,12 @@ class MostGovernorateModel extends MostGovernorateEntity {
 
   factory MostGovernorateModel.fromJson(Map<String, dynamic> json) {
     return MostGovernorateModel(
-      id: json['_id'],
-      provinceId: json['province_id'],
-      governorateNameAr: json['governorate_name_ar'],
-      governorateNameEn: json['governorate_name_en'],
+      id: json['_id'] ?? json['id'] ?? '',
+      provinceId: json['province_id'] ?? 0,
+      governorateNameAr:
+          json['governorate_name_ar'] ?? json['governorateNameAr'] ?? '',
+      governorateNameEn:
+          json['governorate_name_en'] ?? json['governorateNameEn'] ?? '',
     );
   }
 }
@@ -122,10 +141,10 @@ class MostCityModel extends MostCityEntity {
 
   factory MostCityModel.fromJson(Map<String, dynamic> json) {
     return MostCityModel(
-      id: json['_id'],
-      governorateId: json['governorate_id'],
-      cityNameAr: json['city_name_ar'],
-      cityNameEn: json['city_name_en'],
+      id: json['_id'] ?? json['id'] ?? '',
+      governorateId: json['governorate_id'] ?? 0,
+      cityNameAr: json['city_name_ar'] ?? json['cityNameAr'] ?? '',
+      cityNameEn: json['city_name_en'] ?? json['cityNameEn'] ?? '',
     );
   }
 }
@@ -139,9 +158,9 @@ class MostSubCategoryModel extends MostSubCategoryEntity {
 
   factory MostSubCategoryModel.fromJson(Map<String, dynamic> json) {
     return MostSubCategoryModel(
-      id: json['_id'],
-      nameAr: json['nameAr'],
-      nameEn: json['nameEn'],
+      id: json['_id'] ?? json['id'] ?? '',
+      nameAr: json['nameAr'] ?? '',
+      nameEn: json['nameEn'] ?? '',
     );
   }
 }
